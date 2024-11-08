3f3992d3c89c73b589c18b7c2cd641c0549e762a
waterhumanB <dpfflsk45@gmail.com>
2024-11-08 17:24:27 +0900

    feat: 재시작 기능 완성, reset 함수 만들어 게임 재시작 여부에 따리 play함수를 콜백으로 받음

--- src/index.js
+++ src/index.js
@@ -17,13 +17,25 @@ function readLineAsync(query) {
   });
 }
 
+async function reset(callback) {
+  const resetGame = await readLineAsync("게임을 다시 시작하시겠습니까? (yes/no): ");
+  if(resetGame != "yes" && resetGame != "no") {
+    console.log("yes 또는 no만 입력해주세요.")
+    return await reset()
+  }
+  if(resetGame == "yes") {
+    return callback()
+  }
+  return
+}
+
 // 입출력 예시
 async function play() {
 	let answer = []
   console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
   const correctAnswer = Math.ceil(Math.random() * 50)
   
-  while ( answer.length < 100){
+  while ( answer.length < 6){
     const inputValue = await readLineAsync("숫자 입력: ");
     answer.push(inputValue)
 
@@ -35,19 +47,28 @@ async function play() {
     {
       console.log("업")
       console.log("이전 추축:", ...answer)
+      console.log("")
     }
     if(answer[answer.length-1]  > correctAnswer && inputValue <= 50)
     {
       console.log("다운")
       console.log("이전 추축:", ...answer)
+      console.log("")
     }
     if(answer[answer.length-1] == correctAnswer && inputValue <= 50 )
     {
       console.log("정답!")
-      return  console.log(`축하합니다! ${answer.length}번 만에 맞추셨습니다.`)
+      console.log(`축하합니다! ${answer.length}번 만에 맞추셨습니다.`)
+      console.log("")
+      return await reset(play)
     }
   }
 
+  if(answer.length > 5 ){
+    console.log(`5회 초과! 숫자를 맞추지 못했습니다! (정답: ${correctAnswer})`)
+    console.log("")
+    return await reset(play)
+  }
 }
 
 play();
