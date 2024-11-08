5b5aa3ca7900709e15ac0116462d8acf2074f65d
waterhumanB <dpfflsk45@gmail.com>
2024-11-08 16:44:52 +0900

    feat: 사용자 입력에 따른 업,다운,정답 출력 및 50이상 입력시 1~50까지 입력하라는 출력 및 사용자 이전 추측 및 숫자 입력 횟수 최대 5번 및 정답 맞추고 몇회만에 맞췄는지 출력 완성

--- src/index.js
+++ src/index.js
@@ -19,10 +19,35 @@ function readLineAsync(query) {
 
 // 입출력 예시
 async function play() {
-	console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
-  const answer = Math.ceil(Math.random() * 50)
-  const inputValue = await readLineAsync("숫자 입력: ");
+	let answer = []
+  console.log("컴퓨터가 1~50 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요.");
+  const correctAnswer = Math.ceil(Math.random() * 50)
   
+  while ( answer.length < 100){
+    const inputValue = await readLineAsync("숫자 입력: ");
+    answer.push(inputValue)
+
+    if (inputValue > 50) {
+      console.log("숫자는 1 ~ 50 까지만 입력해주세요.")
+      answer.pop()
+    }
+    if(answer[answer.length-1] < correctAnswer && inputValue <= 50)
+    {
+      console.log("업")
+      console.log("이전 추축:", ...answer)
+    }
+    if(answer[answer.length-1]  > correctAnswer && inputValue <= 50)
+    {
+      console.log("다운")
+      console.log("이전 추축:", ...answer)
+    }
+    if(answer[answer.length-1] == correctAnswer && inputValue <= 50 )
+    {
+      console.log("정답!")
+      return  console.log(`축하합니다! ${answer.length}번 만에 맞추셨습니다.`)
+    }
+  }
+
 }
 
 play();
