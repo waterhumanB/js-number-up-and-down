fdc4e62d927f677407ca76d2ba65b135bc089f05
waterhumanB <dpfflsk45@gmail.com>
2024-11-18 23:08:03 +0900

    feat: 최소,최대 값 받기 및 게임 횟수 받기 ui와 domain 분리

---
+++ domain/validation.js
@@ -0,0 +1,20 @@
+export function validateMinMax(input) {
+const parts = input.split(",")
+
+  if (parts.length === 2 && parts.every((part) => part !== "" && Number(part)) && Number(parts[0]) < Number(parts[1])) {
+    const min = Number(input.split(",")[0]?.trim())
+    const max = Number(input.split(",")[1]?.trim())
+
+    return {min, max}
+  }
+
+  return null
+}
+
+export function validateCount(input) {
+  if ( input !== "" && Number(input) > 0 && !isNaN(input) ){
+    return Number(input)
+  }
+
+  return null
+}
--- view/startGame.js
+++ view/startGame.js
@@ -1,52 +1,38 @@
 import readLineAsync from "./readLineAsync.js";
+import { validateCount, validateMinMax } from "../domain/validation.js";
 
 async function getMinMaxInput() {
-  let isValid = false
   let inputValue
+  let result
 
-  while (!isValid){
+  while (!result){
     console.log("[게임 설정] 게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)")
     inputValue = await readLineAsync("숫자 입력: ");
-    const parts = inputValue.split(",")
-    console.log(parts)
-
-    if (parts.length === 2 && parts.every((part) => part !== "" && Number(part)) && Number(parts[0]) < Number(parts[1])){
-      console.log(inputValue)
-      isValid = true
-    } 
-
-    if (isValid === false){
+    result = validateMinMax(inputValue)
+    
+    if (!result){
       console.log("잘못된 형식입니다! 게임 설정에 맞게 입력해주세요.")
     }
   }
   
-  const minValue = Number(inputValue.split(",")[0]?.trim())
-  const maxValue = Number(inputValue.split(",")[1]?.trim())
-
-  return { minValue, maxValue}
+  return result
 }
 
 async function getPlayCount() {
-  let isValid = false
   let inputValue
+  let result
 
-  while (!isValid){
+  while (!result){
     console.log("[게임 설정] 게임 시작을 위해 진행 가능 횟수를 입력해주세요. (예: 5)")
     inputValue = await readLineAsync("숫자 입력: ");
-
-    if ( inputValue !== "" && Number(inputValue) > 0 && !isNaN(inputValue) ){
-      console.log(inputValue)
-      isValid = true
-    }
-
-    if (isValid === false) {
+    result = validateCount(inputValue)
+    
+    if (!result) {
       console.log("잘못된 입력입니다! 게임 설정에 맞게 입력해주세요.")
     }
-
-    return inputValue
   }
 
-  return inputValue
+  return result
 }
 
 
@@ -58,5 +44,4 @@ async function startGame() {
 }
 
 // export default startGame
-
 startGame()
