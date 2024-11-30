8b6200006f28d90b71364274593eaf18005733eb
waterhumanB <dpfflsk45@gmail.com>
2024-11-30 18:34:43 +0900

    feat: view 로직 변경

--- view/view.js
+++ view/view.js
@@ -1,78 +1,108 @@
-import readline from "readline";
+// import readline from "readline";
 
-export class GameView {
-  async readLineAsync(query) {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+// export class GameView {
+//   async readLineAsync(query) {
+//     const rl = readline.createInterface({
+//       input: process.stdin,
+//       output: process.stdout,
+//     });
 
-    return new Promise((resolve) => {
-      rl.question(query, (input) => {
-        resolve(input);
-        rl.close(); // 입력 후 바로 닫음
-      });
-    });
-    }
+//     return new Promise((resolve) => {
+//       rl.question(query, (input) => {
+//         resolve(input);
+//         rl.close(); // 입력 후 바로 닫음
+//       });
+//     });
+//     }
   
+//   displayError (message) {
+//     return console.log(message)
+//   }
+
+//   async getGameConfigMinMax() {
+//     console.log("[게임 설정] 게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)")
+//     const minMaxValue = await this.readLineAsync("숫자 입력: ");
+  
+//     return minMaxValue
+//   }
+
+//   async getGameConfigMaxAttempts() {
+//     console.log("[게임 설정] 게임 시작을 위해 진행 가능 횟수를 입력해주세요. (예: 5)")
+//     const maxAttempts = await this.readLineAsync("숫자 입력: ");
+  
+//     return maxAttempts
+//   }
+
+//   async getGuess() {
+//     const countValue = await this.readLineAsync("숫자 입력: ");
+  
+//     return Number(countValue) 
+//   }
+
+//   displayGameStart(min, max) {
+//     return console.log(`[게임시작] ${min}~${max} 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요!`);
+//   }
+
+//   displayResult(result, attempts, answer) {
+//     switch (result) {
+//       case 'UP':
+//         console.log('업');
+//         console.log('이전 추측:', attempts.join(', '));
+//         break;
+//       case 'DOWN':
+//         console.log('다운');
+//         console.log('이전 추측:', attempts.join(', '));
+//         break;
+//       case 'CORRECT':
+//         console.log(`정답!\n축하합니다! ${attempts.length}번 만에 맞추셨습니다.`);
+//         break;
+//       case 'EXCEEDED':
+//         console.log(`${attempts.length}회 초과! 숫자를 맞추지 못했습니다! (정답: ${answer})`);
+//         break;
+//     }
+//   }
+
+//   async askPlayAgain() {
+//     const resetGame = await this.readLineAsync("게임을 다시 시작하시겠습니까? (yes/no): ");
+
+//     if (resetGame !== "yes" && resetGame !== "no") {
+//       console.log("yes 또는 no만 입력해주세요.")
+//       return await this.askPlayAgain()
+//     }
+
+//     if (resetGame == "yes") {
+//       return resetGame
+//     }
+//   }
+// }
+
+class InputHandler {
+
   displayError (message) {
-    return console.log(message)
+    return window.alert(message)
   }
 
-  async getGameConfigMinMax() {
-    console.log("[게임 설정] 게임 시작을 위해 최소 값, 최대 값을 입력해주세요. (예: 1, 50)")
-    const minMaxValue = await this.readLineAsync("숫자 입력: ");
+  getInputValue (inputId) {
+    return document.getElementById(inputId).value
+  }
+
+  displayMsg (elId, msg) {
+    return document.getElementById(elId).ELEMENT_NODE.innerText = msg
+  }
+
+
+  clickButton(btnId, callback) {
+    const button = document.getElementById(btnId);
+
+    button.addEventListener("click", () => {
+      callback(); // 동적으로 전달된 로직 실행
+    });
+  }
   
-    return minMaxValue
-  }
-
-  async getGameConfigMaxAttempts() {
-    console.log("[게임 설정] 게임 시작을 위해 진행 가능 횟수를 입력해주세요. (예: 5)")
-    const maxAttempts = await this.readLineAsync("숫자 입력: ");
-  
-    return maxAttempts
-  }
-
-  async getGuess() {
-    const countValue = await this.readLineAsync("숫자 입력: ");
-  
-    return Number(countValue) 
-  }
-
-  displayGameStart(min, max) {
-    return console.log(`[게임시작] ${min}~${max} 사이의 숫자를 선택했습니다. 숫자를 맞춰보세요!`);
-  }
-
-  displayResult(result, attempts, answer) {
-    switch (result) {
-      case 'UP':
-        console.log('업');
-        console.log('이전 추측:', attempts.join(', '));
-        break;
-      case 'DOWN':
-        console.log('다운');
-        console.log('이전 추측:', attempts.join(', '));
-        break;
-      case 'CORRECT':
-        console.log(`정답!\n축하합니다! ${attempts.length}번 만에 맞추셨습니다.`);
-        break;
-      case 'EXCEEDED':
-        console.log(`${attempts.length}회 초과! 숫자를 맞추지 못했습니다! (정답: ${answer})`);
-        break;
-    }
-  }
-
-  async askPlayAgain() {
-    const resetGame = await this.readLineAsync("게임을 다시 시작하시겠습니까? (yes/no): ");
-
-    if (resetGame !== "yes" && resetGame !== "no") {
-      console.log("yes 또는 no만 입력해주세요.")
-      return await this.askPlayAgain()
-    }
-
-    if (resetGame == "yes") {
-      return resetGame
-    }
-  }
 }
+const handler = new InputHandler();
 
+// 버튼 클릭 이벤트 추가
+handler.clickButton("start", () =>{
+  console.log(handler.getInputValue("min"))
+})
